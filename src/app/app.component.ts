09df56b0a7c0a5570e990ac2efc923befcc8e56a
raj1020 <raj.1020@hotmail.com>
2020-03-12 21:51:49 +1100

    data binding to model performed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {User } from './user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('Rob', 'rob@test.com', 43843628376, '', "morning", true);
 }
